87472e4fa076ea7456d0fb64cdb26d328ea4f34e
choqueuse <vincent.choqueuse@enib.fr>
2020-11-09 14:28:29 +0100

    s3

--- click_collect/click_collect/settings.py
+++ click_collect/click_collect/settings.py
@@ -139,7 +139,6 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'eu-west-3' #change to your region
-AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals(),staticfiles=False)
