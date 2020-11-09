8a4d64987b0fb769308dc331103d0042a67f83d6
choqueuse <vincent.choqueuse@enib.fr>
2020-11-09 09:50:46 +0100

    procfile

--- click_collect/click_collect/settings.py
+++ click_collect/click_collect/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'click_collect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["templates",os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
